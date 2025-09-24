2dc18feccebe7b26a05fd6bdcbe96c55431d04cf
Eden Leyba <edenleyba44@gmail.com>
2025-09-24 05:06:38 +0300

    article schema

---
+++ models/article.model.ts
@@ -0,0 +1,24 @@
+import {Schema, model, Document} from 'mongoose';
+
+interface IArticle extends Document {
+    title: string;
+    content: string;
+    author: string;
+    tags: string[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const articleSchema = new Schema<IArticle>(
+    {
+        title: {type: String, required: true},
+        content: {type: String, required: true},
+        author: {type: String, required: true},
+        tags: {type: [String], default: []}
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Article = model<IArticle>('Article', articleSchema);
